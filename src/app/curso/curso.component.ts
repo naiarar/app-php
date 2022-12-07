25bb08856432a385ef05b1d78f4f325512cd5c85
Naiara Rodrigues <rnaiara92@gmail.com>
2022-12-07 13:02:22 -0300

    criação de funçoes

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-curso',
   templateUrl: './curso.component.html',
   styleUrls: ['./curso.component.css']
 })
-export class CursoComponent {
+export class CursoComponent implements OnInit {
+
+  nome: string = "Naiara";
+  //Contrutor
+  constructor() {
+
+  }
+  //Inicializador
+  ngOnInit() {
+  }
+
+  cadastro(): void {
+    alert("cadastro");
+  }
+
+  selecao(): void {
+    alert("selecao");
+  }
+
+  alterar(): void {
+    alert("alterar");
+  }
+
+  remover(): void {
+    alert("remover");
+  }
 
 }
