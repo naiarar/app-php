6c02724acd58b1a240977a2c564c4991211e0add
Naiara Rodrigues <rnaiara92@gmail.com>
2022-12-08 11:52:03 -0300

    vetor ok

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from './curso';
+import { CursoService } from './curso.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-curso',
@@ -7,29 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoComponent implements OnInit {
 
-  nome: string = "Naiara";
-  //Contrutor
-  constructor() {
+  //Vetor
+  vetor!: Curso[];
+
+  //Objeto da classe Curso
+  curso = new Curso();
+
+  //Construtor
+  constructor(private curso_servico: CursoService) {
 
   }
   //Inicializador
   ngOnInit() {
+    //Ao iniciar o sistema deverá listar os cursos
+    this.selecao();
   }
 
-  cadastro(): void {
-    alert("cadastro");
+  selecao() {
+    this.curso_servico.obterCursos().subscribe(
+      (res: Curso[]) => {
+        this.vetor = res;
+
+      }
+    )
   }
 
-  selecao(): void {
-    alert("selecao");
+
+  cadastro() {
+    this.curso_servico.cadastrarCurso(this.curso).subscribe(
+      (res: Curso[]) => {
+        //adc dados ao vetor
+        this.vetor = res;
+
+        //limpar os atributos
+        this.curso.nomeCurso
+        this.curso.valorCurso
+
+        //atualizar listagem
+        this.selecao();
+      }
+    )
+
   }
 
-  alterar(): void {
-    alert("alterar");
-  }
 
-  remover(): void {
-    alert("remover");
-  }
+  // alterar(): void {
+  //     alert("alterar");
+  //   }
+
+  // remover(): void {
+  //     alert("remover");
+  //   }
 
 }
