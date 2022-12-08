f201089a8498906b0ab8adf08376812ca0c92769
Naiara Rodrigues <rnaiara92@gmail.com>
2022-12-08 15:41:05 -0300

    atribulos ok

--- src/app/curso/curso.ts
+++ src/app/curso/curso.ts
@@ -1,7 +1,6 @@
 export class Curso {
 
-    constructor(nomeCurso: string, valorCurso: number, idCurso?: number) {
-
-    }
+    //Atributos
+    nomeCurso?: string; valorCurso?: number; idCurso?: number;
 
 }
